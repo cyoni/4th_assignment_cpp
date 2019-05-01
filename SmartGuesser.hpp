ab7ddf3fd977741f883ac11d3b75d233b2fa1725
cyoni <44746539+cyoni@users.noreply.github.com>
2019-05-01 15:20:47 +0300

    Add files via upload

--- SmartGuesser.hpp
+++ SmartGuesser.hpp
@@ -13,11 +13,29 @@ namespace bullpgia{
 class SmartGuesser : public bullpgia::Guesser { // son that extends guesser
 
 private:
-   
+    int dig_Counter;
+    int look_counter;
+    int bul;
+    int looking;
+    int found;
+
+    bool check_if_not_in_list(int,int);
+    vector<list<int>> not_in_place;
+    vector<int>certain;
+    string bAndp;
     string guess();
+    string give_sequence();
     void learn(string);
     void startNewGame(uint);
- 
+    int Try;
+
+    string Guess;
+
+
+
+
+protected: // can be accessed only by base members, friends and derived classes
+
 
 public:
 	SmartGuesser();
@@ -25,3 +43,4 @@ public:
 };
 
 }
+
